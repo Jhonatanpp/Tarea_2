81a00cc5880482c071d6edeb51dc581a4b29b27d
Joseph <joquevedor@unal.edu.co>
2025-04-28 15:42:21 -0500

    Esta modificacion incluye conversion de INT a STRING, profundiza en primitivos y objetos

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 En este proyecto se presentan ejemplos básicos de cómo convertir datos primitivos (int, float, double) a objetos (Integer, Float, Double) en Java.
 La aplicación consiste en una pequeña encuesta donde se solicitan nombre, edad, carrera y número de semestre, mostrando cómo realizar dichas conversiones de forma sencilla utilizando valueOf y autoboxing.
+
+La segunda conversion consiste en transformar un valor primitivo a objeto y luego modificar eltipo de dato,en este caso, En Int como contable para pasarlo a texto como un string
--- src/tarea2/Tarea2.java
+++ src/tarea2/Tarea2.java
@@ -7,7 +7,7 @@ package tarea2;
 import java.util.Scanner;
 
 /**
- *
+ * @author Joseph Quevedo
  * @author jhonatan Pedraza
  */
 public class Tarea2 {
@@ -24,8 +24,10 @@ public class Tarea2 {
     
     // Ejemplo de conversión de tipos primitivos a objetos en Java usando una encuesta.
     public static void main(String[] args) {
+        
+        
          Scanner scanner = new Scanner(System.in);
-
+         
         // Solicitar datos al usuario
         System.out.print("Introduce tu nombre: ");
         String nombre = scanner.nextLine();
@@ -35,7 +37,27 @@ public class Tarea2 {
         Integer edadObjeto = Integer.valueOf(edadPrimitivo); // Conversión de primitivo a objeto
     //Jhonatan//
     
+    
+    
+    //Joseph//
+    
+    //Conversion de un dato Int a String basado en tu edad//
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter your age: "); //SOLICITA TU EDAD Y LA ESCANEA
+        
+        int edad = s.nextInt();  //Se almacena el primitivo como Int
+        
+        String EDADentexto = String.valueOf(edad); //Hacemos la conversion del objeto a String
+        
+        System.out.println("Your age written in text is: " + EDADentexto + "."); //Retornamos los datos
+        
+    //Joseph//
+    
+    
+    
+    
     //Compas pongan otras clases aqui abajo otros datos
+    
     }
     
 }
